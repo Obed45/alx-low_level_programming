b4dcdff7af01be4d9fb5c41054653f223424c490
Obed45 <adjeijason68@gmail.com>
2022-12-08 01:31:06 -0800

    task 5

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * 
+ * Retune: Always 0 (Success)
+ */
+
+ int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
+  
