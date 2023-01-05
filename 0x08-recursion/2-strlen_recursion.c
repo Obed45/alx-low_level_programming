77469909596da211650084483d3aa31e7b16a302
Obed45 <adjeijason68@gmail.com>
2023-01-05 03:24:03 -0800

    task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strlen_recursion - Returns the length of a string.
+ * _strlen_recursion - returns the length of a string.
  * @s: string
  * Return: the length of a string.
  */
