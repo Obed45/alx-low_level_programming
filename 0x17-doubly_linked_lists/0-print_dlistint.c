6c8c5a168a2a7f47eb83f350210c4e1bb2b6101c
Obed45 <adjeijason68@gmail.com>
2023-03-16 08:52:24 -0700

    ups2

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
  * print_dlistint - prints all the elements of a dlistint_t list
  * @h: pointer to the head of the list
@@ -8,14 +7,12 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t count = 0;
-
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        count++;
-    }
-
-    return (count);
+size_t count = 0;
+while (h != NULL)
+{
+printf("%d\n", h->n);
+h = h->next;
+count++;
+}
+return (count);
 }
---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - returns the nth node of a dlistint_t linked list
+ * @head: pointer to the head of the list
+ * @index: index of the node, starting from 0
+ *
+ * Return: pointer to the nth node, or NULL if the node does not exist
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+    dlistint_t *current = head;
+    unsigned int count = 0;
+
+    while (current != NULL)
+    {
+        if (count == index)
+            return (current);
+
+        count++;
+        current = current->next;
+    }
+
+    return (NULL);
+}
